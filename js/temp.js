9aa6402390d60899a82d364a9383c387b9d54376
Dmitry <katashinskydmitry@gmail.com>
2018-09-04 16:45:02 +0300

    almost ended project or already ended

--- js/temp.js
+++ js/temp.js
@@ -1,13 +1,18 @@
 let list = document.querySelector(".list-group");
 let clear = document.getElementById("clear");
-let inputText = document.getElementById("inputText");
-let addText = document.getElementById("addText");
 let form = document.forms["add-text-form"];
+let addText = form.elements["addText"];
+let inputText = form.elements["inputText"];
+let notificaitonAlert = document.querySelector(".notification-alert");
 
-let array = [{
-    id: "1",
-    text: "fucking message"
-}];
+let array = [];
+
+function getFromLocalStorage() {
+    let parseArray = JSON.parse(localStorage.task);
+    for (let i = 0; i < parseArray.length; i++) {
+        array.unshift(parseArray[i]);
+    }
+}
 
 function addElementToArray() {
     let id = generatedId();
@@ -16,6 +21,7 @@ function addElementToArray() {
     obj.id = id;
     obj.text = text;
     array.unshift(obj);
+    localStorage.task = JSON.stringify(array);
 }
 
 function generatedId() {
@@ -28,15 +34,22 @@ function generatedId() {
     return id;
 }
 
-function addElement(elem) {
+function addElement(elem, id) {
     let li = document.createElement("li");
     li.className = "list-group-item d-flex";
+
+    let span = document.createElement("span");
+    span.textContent = elem;
+
     let iconDelete = document.createElement("i");
     iconDelete.className = "fas fa-trash-alt delete ml-2";
+
     let iconEdit = document.createElement("i");
     iconEdit.className = "fas fa-edit edit ml-auto";
-    li.setAttribute("data-id", generatedId().toString());
-    li.textContent = elem;
+
+    li.setAttribute("data-id", id);
+
+    li.appendChild(span);
     li.appendChild(iconEdit);
     li.appendChild(iconDelete);
     list.appendChild(li);
@@ -45,7 +58,7 @@ function addElement(elem) {
 function createList(){
     clearList();
     for (let i = 0; i < array.length; i++) {
-        addElement(array[i].text);
+        addElement(array[i].text, array[i].id);
     }
 }
 
@@ -53,13 +66,99 @@ function clearList() {
     list.innerHTML = '';
 }
 
-clear.addEventListener("click", clearList);
+function deleteElem(elem){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i].id === elem){
+            array.splice(i,1);
+            localStorage.task = JSON.stringify(array);
+            break;
+        }
+    }
+
+    message({
+        text: "message removed",
+        cssClass: "alert-danger",
+        timeOut: 4000
+    });
+}
+
+function editElement(elem, text){
+    for (let i = 0; i < array.length; i++) {
+        if(array[i].id === elem){
+            array[i].text = text;
+            localStorage.task = JSON.stringify(array);
+            break;
+        }
+    }
+
+    message({
+        text: "message saved",
+        cssClass: "alert-success",
+        timeOut: 4000
+    });
+}
+
+function message(setting){
+    notificaitonAlert.classList.add(setting.cssClass);
+    notificaitonAlert.textContent = setting.text;
+    notificaitonAlert.classList.add("show");
+
+    setTimeout(function () {
+        notificaitonAlert.classList.remove("show");
+    },setting.timeOut)
+
+}
+
+clear.addEventListener("click", () => {
+    clearList();
+    array = [];
+});
 
 addText.addEventListener("click", function (e) {
     e.preventDefault();
+    if(inputText.value !== ''){
+        addElementToArray();
+        createList();
+        inputText.value = '';
 
-    addElementToArray();
-    createList();
-    inputText.value = '';
+        message({
+            text: "message was added",
+            cssClass: "alert-success",
+            timeOut: 4000
+        });
+
+    }else {
+        inputText.classList.add("is-invalid");
+    }
+
 });
+
+inputText.addEventListener("keyup", () => inputText.classList.remove("is-invalid"));
+
+list.addEventListener("click", function (e) {
+    let elem = e.target;
+
+    if(elem.classList.contains("delete")){
+        deleteElem(elem.parentElement.dataset.id);
+
+        elem.parentElement.remove();
+
+    }else if(elem.classList.contains("edit")){
+
+        elem.classList.toggle("fa-save");
+
+        let prev = elem.parentElement.firstChild;
+        prev.setAttribute("contenteditable", "true");
+
+        prev.focus();
+
+        if(!elem.classList.contains("fa-save")){
+            editElement(elem.parentElement.dataset.id, prev.textContent);
+
+            prev.blur();
+        }
+    }
+});
+
+getFromLocalStorage();
 createList();
